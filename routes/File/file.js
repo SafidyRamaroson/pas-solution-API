7af6f52b8c215c8b0a37b82d87dbc3f8a0c47cbb
Safidy RAMAROSON <safidyramaroson.patrick@gmail.com>
2023-12-15 17:04:06 +0300

    fixed syntax error

--- routes/File/file.js
+++ routes/File/file.js
@@ -3,6 +3,6 @@ import { getDriveFilesAndStoreToDB } from './../../controllers/fileController.js
 import { retrieveFileFromDatabase } from './../../controllers/fileController.js';
 
 const router = express.Router();
-router.get('/getFilesFromDabatase',getDriveFilesAndStoreToDB,retrieveFileFromDatabase)
+router.get('/getFilesFromDatabase',getDriveFilesAndStoreToDB,retrieveFileFromDatabase)
 
 export default router ;
